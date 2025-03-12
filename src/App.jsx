82c76c85316f49d92cad9f0df601e1b8a42eb329
Splayer1273 <sonvanesiddesh@gmail.com>
2025-03-12 22:28:44 +0530

    frontend

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <nav className='bg-black flex items-center justify-between px-4'>
+      <div className='bg-white size-12 flex items-center justify-center rounded-4xl '>logo</div>
+      <button className='bg-blue-600 text-white border-r-blue p-2 rounded-md' >Submit</button>
+
+    </nav>
+  )
+}
+
+export default App
